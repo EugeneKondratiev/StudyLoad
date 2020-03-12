b7ccdc023793d47800dedfc1373e308eb475f56d
EugeneKondratiev <albrus10@mail.ru>
2020-03-12 21:28:39 +0200

    Output string

--- src/main/java/modules/HelloParse.java
+++ src/main/java/modules/HelloParse.java
@@ -6,10 +6,7 @@ import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.ss.usermodel.*;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -18,10 +15,12 @@ import java.util.Date;
 
 public class HelloParse {
     public void readFromExcel(String file) throws IOException, InvalidFormatException {
-        Workbook wb = WorkbookFactory.create(new File("C:\\Users\\GreamReaper\\Downloads\\ІТтаКБ. Сем I. Форма навчання  денна.xlsx"));
+        Workbook wb = WorkbookFactory.create(new File("C:\\Users\\albru\\Documents\\java\\StudyLoad\\src\\main\\resources\\ІТтаКБ. Сем I. Форма навчання  денна.xlsx"));
         Sheet myExcelSheet = wb.getSheet("Робота кафедри");
-        Row row = myExcelSheet.getRow(0);
-        System.out.println(row);
+        Row row =  myExcelSheet.getRow(0);
+        Cell cell = row.getCell(0);
+        RichTextString richStringCellValue = cell.getRichStringCellValue();
+        System.out.println(richStringCellValue);
 //        if (row.getCell(0).getCellType() == HSSFCell.CELL_TYPE_STRING) {
 //            String name = row.getCell(0).getStringCellValue();
 //            System.out.println("name : " + name);
