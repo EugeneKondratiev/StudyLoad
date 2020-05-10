9a8b81999fb5714c035965dab821709ea86af560
EugeneKondratiev <albrus10@mail.ru>
2020-05-10 23:29:06 +0300

    output stream was added

--- src/main/java/modules/HelloParse.java
+++ src/main/java/modules/HelloParse.java
@@ -7,19 +7,27 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
 
 public class HelloParse {
     public void readFromExcel(String file) throws IOException, InvalidFormatException {
-        Workbook wb = WorkbookFactory.create(new File("src/main/resources/ІТтаКБ. Сем I. Форма навчання  денна.xlsx"));
+        FileInputStream inputStream = new FileInputStream(new File(file));
+        Workbook wb = WorkbookFactory.create(inputStream);
         Sheet myExcelSheet = wb.getSheet("Робота кафедри");
         Row row =  myExcelSheet.getRow(0);
         Cell cell = row.getCell(0);
+        cell.setCellValue("Set value");
+        //Workbook wbTest = WorkbookFactory.create(new File("src/main/resources/ІТтаКБ. Сем I. Форма навчання  денна.xlsx"));
+        FileOutputStream outputStream = new FileOutputStream(file);
+        wb.write(outputStream);
         RichTextString richStringCellValue = cell.getRichStringCellValue();
+
         System.out.println(richStringCellValue);
 //        if (row.getCell(0).getCellType() == HSSFCell.CELL_TYPE_STRING) {
 //            String name = row.getCell(0).getStringCellValue();
@@ -32,5 +40,6 @@ public class HelloParse {
 //        }
 
         wb.close();
+        outputStream.close();
     }
 }
--- src/main/java/modules/Main.java
+++ src/main/java/modules/Main.java
@@ -1,14 +1,47 @@
 package modules;
 
-import org.apache.log4j.Logger;
+import org.apache.poi.hssf.eventusermodel.HSSFEventFactory;
+import org.apache.poi.hssf.eventusermodel.HSSFRequest;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Main {
     public static void main(String[] args) throws IOException, InvalidFormatException {
         HelloParse helloParse = new HelloParse();
-        helloParse.readFromExcel("File");
+        helloParse.readFromExcel("src/main/resources/ІТтаКБ. Сем I. Форма навчання  денна.xlsx");
 
+        // create a new file input stream with the input file specified
+//        FileInputStream fileInputStream = new FileInputStream("src/main/resources/ІТтаКБ. Сем I. Форма навчання  денна.xlsx");
+//
+//        // create a new org.apache.poi.poifs.filesystem.Filesystem
+//        POIFSFileSystem poifsFileSystem = new POIFSFileSystem(fileInputStream);
+//
+//
+//        // get the Workbook (excel part) stream in a InputStream
+//        InputStream inputStream = poifsFileSystem.createDocumentInputStream("Workbook");
+//
+//        // construct out HSSFRequest object
+//        HSSFRequest hssfRequest = new HSSFRequest();
+//
+//        // lazy listen for ALL records with the listener shown above
+//        hssfRequest.addListenerForAllRecords(new ExcelListener());
+//
+//        // create our event factory
+//        HSSFEventFactory factory = new HSSFEventFactory();
+//
+//        // process our events based on the document input stream
+//        factory.processEvents(hssfRequest, inputStream);
+//
+//        // once all the events are processed close our file input stream
+//        fileInputStream.close();
+//
+//        // and our document input stream (don't want to leak these!)
+//        inputStream.close();
+//
+//        System.out.println("done.");
     }
 }
